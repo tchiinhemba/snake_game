f0ecd15f390a59991f6fbcf9cec1133ae205586b
Eládio Tchiinhemba <tchiinhemba@outlook.com>
2020-09-23 02:05:08 +0100

    update

--- js/script.js
+++ js/script.js
@@ -3,6 +3,14 @@
 let canvas = document.querySelector('#snake');
 let context = canvas.getContext('2d');
 let box = 32;
+let snake = [];
+
+snake[0] = {
+    x: 8 * box,
+    y: 8 * box
+}
+
+let direction = 'right';
 
 function createBG(){
     context.fillStyle = 'lightgreen';
@@ -11,6 +19,11 @@ function createBG(){
 
 function createSnake(){
     
+    for ( i = 0; i < snake.length; i++){
+        context.fillStyle = 'green';
+        context.fillRect(snake[i].x, snake[i].y, box, box);
+    }
 }
 
 createBG();
+createSnake();
