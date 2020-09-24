546a0ea7bc5a529b6a9264e728f3a2437ae6825e
Tchiinhemba <tchiinhemba@outlook.com>
2020-09-24 10:45:34 +0100

    update

--- js/script.js
+++ js/script.js
@@ -25,6 +25,16 @@ function createSnake(){
     }
 }
 
+document.addEventListener('keydown', update);
+
+function update(event) {
+
+    if (event.keyCode == 37 && direction != 'right') direction = 'left';
+    if (event.keyCode == 38 && direction != 'down') direction = 'up';
+    if (event.keyCode == 39 && direction != 'left') direction = 'right';
+    if (event.keyCode == 40 && direction != 'up') direction = 'down';
+}
+
 
 function startGame(){
 
