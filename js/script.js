d5c69b3fb2b413ff542457c9ec6392cb304b5da8
Tchiinhemba <tchiinhemba@outlook.com>
2020-09-25 04:22:06 +0100

    add title

--- js/script.js
+++ js/script.js
@@ -61,7 +61,7 @@ function startGame(){
 
         if (snake[0].x == snake[i].x && snake[0].y == snake[i].y){
             clearInterval(game);
-            alert('Game Over :(');
+            alert('Game Over :('); 
         }
     }
 
